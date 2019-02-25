f177430951332db9f313ef2e6f1a5d2d2559ba1a
3Nd_R1m <ben.bergeron1@gmail.com>
2019-02-25 14:16:50 -0500

    Did first function and misc.

--- src/index.php
+++ src/index.php
@@ -1,21 +1,6 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<meta charset="utf-8">
-	<title><?php echo PAGE_TITLE?></title>
-	<meta name="author" content="Benjamin Bergeron, James Martin">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
-</head>
-<body>
-	<header>
-		InterCarRent
-	</header>
-	<?php
-	
-	?>
-	<footer>
-		Copyright interCarRent <?php echo date("Y"); ?>  
-	</footer>
-</body>
-</html>
+<?php
+    class Controller
+    {
+        
+    }
+?>
--- src/models/financement.php
+++ src/models/financement.php
@@ -1,3 +1,8 @@
 <?php
 
+function calculateMonthlyPayment($balance, $periods, $interestRate){
+    # https://en.wikipedia.org/wiki/Amortization_%28business%29
+    $monthlyPayment = $loan * (( 1 - pow(1 + $interestRate, -$loanDurationInMonth)) / $interestRate);
+    return $monthlyPayment;
+}
 ?>
